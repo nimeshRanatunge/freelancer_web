c36101ccebe6015241170551609055c4d51894d6
Thisara Ranatunge <2018cs131@stu.ucsc.cmb.ac.lk>
2023-03-27 17:16:57 +0530

    payment stripe

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,6 +1,40 @@
 // import createError from "../utils/createError";
 import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
+import Stripe from "stripe";
+
+export const intent = async (req, res, next) => {
+  const stripe = new Stripe(process.env.STRIPE);
+
+  const gig = await Gig.findById(req.params.id);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+};
+
+
+
 
 export const createOrder = async (req, res, next) => {
   const gig = await Gig.findById(req.params.gigId);
